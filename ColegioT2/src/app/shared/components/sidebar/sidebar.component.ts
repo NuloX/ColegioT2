1c76a5b178f7a70f2cd7155de3d2605d3d04d797
AymarMolina <aymarmolina9@gmail.com>
2024-09-10 21:14:26 -0500

    agregando login

--- ColegioT2/src/app/shared/components/sidebar/sidebar.component.ts
+++ ColegioT2/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { ApiUserDataService } from '../../services/apiUserData.service';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,RouterLinkActive,MatIconModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit{
   sidemenu1Collapsed: boolean = true;
   sidemenu2Collapsed: boolean = true;
+
+  sidemenu1: boolean = false;
+  sidemenu2: boolean = false;
+
+  constructor(private apiDataUser:ApiUserDataService){
+
+  }
+  ngOnInit(): void {
+    
+  }
   toggleCollapse(collapseNumber: number) {
     if (collapseNumber === 1) {
       this.sidemenu1Collapsed = !this.sidemenu1Collapsed;
