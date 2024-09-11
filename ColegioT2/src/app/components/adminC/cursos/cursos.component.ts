59e35e75c582ae09a7bbb68bb98060ed5ace4882
Diego Laura <zzdieguxxzz@gmail.com>
2024-09-10 21:15:48 -0500

    Agregue los cards del profesor

--- ColegioT2/src/app/components/adminC/cursos/cursos.component.ts
+++ ColegioT2/src/app/components/adminC/cursos/cursos.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-cursos',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './cursos.component.html',
   styleUrl: './cursos.component.css'
 })
